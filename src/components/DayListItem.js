8c362c7d3bb195fedecb36030df2b4409ae496d6
Shivam Patel <patelsd96@gmail.com>
2021-02-17 21:41:47 -0500

    making dayListItem dynamic with Test Driven Dev

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -8,10 +8,21 @@ export default function DayListItem(props) {
     'day-list__item--selected': props.selected,
     'day-list__item--full': (props.spots === 0)
   })
+
+  function formatSpots(props){
+    console.log("testing", props)
+    if(props === undefined || props == 0){
+      return "no spots remaining"
+    } else if(props === 1){
+      return "1 spot remaining"
+    } else {
+      return `${props} spots remaining`
+    }
+  }
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots} spots remaining</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 }
