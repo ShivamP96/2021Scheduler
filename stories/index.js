b6529dc0b96352db7558720d06afba65dad75be4
Shivam Patel <patelsd96@gmail.com>
2021-02-23 22:00:41 -0500

    added in the show component

--- stories/index.js
+++ stories/index.js
@@ -14,6 +14,7 @@ import Appointment from "components/Appointment"
 import "components/Appointment/styles.scss"
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty"
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -149,3 +150,11 @@ storiesOf("Button", module)
         />
       ))
       .add("Empty", () => <Empty onAdd={action("onAdd")} />)
+      .add("Show", () => (
+        <Show
+        student="Lydia Miller-Jones"
+        interviewer={interviewer}
+        onEdit={action("onEdit")}
+        onDelete={action("onDelete")}
+        />
+      ))
