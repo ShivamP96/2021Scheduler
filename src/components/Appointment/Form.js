19dbc2814144d9cf99529c395fb43214b2423d7a
Shivam Patel <patelsd96@gmail.com>
2021-02-27 22:13:16 -0500

    added state to the form for onSave

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -36,7 +36,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section classname="appointment__actions">
           <Button onClick={() => cancel()} danger>Cancel</Button>
-          <Button onClick={props.onSave} confirm>Save</Button>
+          <Button onClick={() => props.onSave(name, interviewer)} confirm>Save</Button>
         </section>
       </section>
     </main>
